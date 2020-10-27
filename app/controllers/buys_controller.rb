e5ba8e5822a07723cbc17a033469ea82429d09cd
Shota-Todoroki <71693760+Shota-Todoroki@users.noreply.github.com>
2020-10-27 11:08:01 +0900

    一部削除

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -22,7 +22,7 @@ class BuysController < ApplicationController
 
   def move_to_root_path
     @product = Product.find(params[:product_id])
-    if @product.user_id == current_user.id || @product.buy.presence#ルーティングでネストしてbuysはproductsの子要素なので、buysテーブルを出力させたい時はこのような記述になる。
+    if @product.user_id == current_user.id || @product.buy.presence
       redirect_to root_path  
     end
   end
